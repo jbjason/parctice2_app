b6646e5a89f4a091277b044033ceaa9472f376e1
Jubayer <62088928+jbjason@users.noreply.github.com>
2023-05-04 16:53:00 +0600

    s

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:parctice2_app/screens/home_screen.dart';
-import 'package:parctice2_app/screens/doc_details_screen.dart';
-import 'package:parctice2_app/screens/soow_home.dart';
 import 'package:parctice2_app/screens/video_call_screen.dart';
 
 class Routes {
@@ -9,13 +7,6 @@ class Routes {
     switch (settings.name) {
       case HomeScreen.routeName:
         return MaterialPageRoute(builder: (context) => const HomeScreen());
-
-      case SooHome.routeName:
-        return MaterialPageRoute(builder: (context) => const SooHome());
-
-      case DocDetailsScreen.routeName:
-        return MaterialPageRoute(
-            builder: (context) => const DocDetailsScreen());
 
       case VideoCallScreen.routeName:
         return MaterialPageRoute(builder: (context) => const VideoCallScreen());
